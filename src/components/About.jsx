2225571c27016fd78e079583c3fd74ce423d09cc
bestkavindu <bestkavindu@gmail.com>
2025-02-22 10:24:07 +0530

    🎨 update about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
-import MernStack from '../assets/mernstack.png'
 import Html from '../assets/Html.png'
 import Css from '../assets/CSS.png'
 import Javascript from '../assets/JS.png'
 import ReactLogo from '../assets/React.png'
-import ReduxLogo from '../assets/Redux.png'
 import Tailwind from '../assets/Tailwind Css.png'
 import Bootstrap from '../assets/Bootstrap.png'
 import NodeLogo from '../assets/NodeLogo.png'
 import Mongodb from '../assets/mongodb.svg'
-import Express from '../assets/Express.png'
-import NextJs from '../assets/NextJs.png'
 import Python from '../assets/Python-Logo.png'
 import PowerBI from '../assets/PowerBI.png'
 import SQL from '../assets/sql.png'
@@ -24,7 +20,7 @@ const AboutComponent = () => {
                         <h2 className='text-base text-red-600 font-semibold tracking-wide uppercase'>About Me</h2>
                         <p className='mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl'>Hi, I'm Sachin Kavindu</p>
                         <p className='mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto'>
-                        Experienced Data Analytics with a strong background in Software Engineering.
+                            Experienced Data Analytics with a strong background in Software Engineering.
                         </p>
                     </div>
                     <div className='mt-10'>
@@ -32,14 +28,13 @@ const AboutComponent = () => {
                             <div>
                                 <h3 className='text-2xl font-semibold text-gray-900'>My Journey</h3>
                                 <p className='mt-4 text-lg text-gray-600'>
-                                    I started my journey in web development with a passion for creating intuitive and scalable applications. With proficiency in the MERN stack (MongoDB, Express.js, React, and Node.js), I have built Projects such as a full-stack food ordering website , ajob portal, and even a LinkedIn clone. My projects demonstrate my ability to integrate poweful backend solutions with sleek, user-friendly frontend designs.
+                                    I started my career as an Associate Software Engineer, where I built intuitive and scalable web applications using Laravel and React. After a year, I transitioned to Clindata Pvt as a Junior Associate Statistical Programmer, focusing on data validation, cleaning, preprocessing, and generating reports through statistical analysis. Combining my expertise in web development and statistical programming, I deliver data-driven solutions with precision and efficiency.
                                 </p>
-                                <img src={MernStack} alt="" className='p-2 rounded-lg w-52 mt-4' />
                             </div>
                             <div className='border border-red-200 rounded-lg md:p-7 py-7  flex flex-col gap-8 items-center shadow-lg shadow-red-300'>
                                 <h3 className='text-2xl font-semibold text-red-600'>Skills & Expertise</h3>
                                 <div className='flex items-center justify-center flex-wrap gap-3'>
-                                <div className='border border-red-300 flex items-center gap-1 w-max px-2 py-1 rounded-lg shadow-md shadow-red-300'>
+                                    <div className='border border-red-300 flex items-center gap-1 w-max px-2 py-1 rounded-lg shadow-md shadow-red-300'>
                                         <img src={Python} alt="" className='w-10' />
                                         <span className='font-semibold'>Python</span>
                                     </div>
@@ -67,7 +62,7 @@ const AboutComponent = () => {
                                         <img src={ReactLogo} alt="" className='w-8 rounded-full' />
                                         <span className='font-semibold'>React</span>
                                     </div>
-    
+
                                     <div className='border border-red-300 flex items-center gap-1 w-max px-2 py-1 rounded-lg shadow-md shadow-red-300'>
                                         <img src={Tailwind} alt="" className='w-8 rounded-full' />
                                         <span className='font-semibold'>Tailwind Css</span>
@@ -84,7 +79,7 @@ const AboutComponent = () => {
                                         <img src={Mongodb} alt="" className='w-10' />
                                         <span className='font-semibold'>Mongodb</span>
                                     </div>
-  
+
                                 </div>
                             </div>
                         </div>
@@ -92,8 +87,7 @@ const AboutComponent = () => {
                     <div className='mt-12'>
                         <h3 className='text-2xl font-semibold text-gray-900'>More About Me</h3>
                         <p className='mt-4 text-lg text-gray-600'>
-                            Apart from coding, I am passionate about learning new technologies and keeping myself up-to-date with the latest trends in web development. I also run a YouTube channel where I teach Javascript, MERN Stack development, Wordpress and digital Marketing, helping others dive deep into the world of programming and technology.
-                        </p>
+                        I'm a passionate coder who loves exploring new technologies and turning ideas into reality. Always eager to learn, I pick up new skills quickly and enjoy creating innovative solutions through coding. Whether it's building web applications or diving into data analysis, I’m driven by the challenge of crafting something new and impactful.                        </p>
                     </div>
                 </div>
             </div>
